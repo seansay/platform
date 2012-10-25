046abd3f62456a1b8531fe4c2eb5a621a103340b
Barrett Griffith <barrett@endpoint.com>
2012-10-25 22:33:52 +0200

    Move logged in user views to be handles by the api_keys_controller

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -7,7 +7,11 @@ class ApiKeysController < ActionController::Base
       status = 200
       application = current_user.api_key.application
       api_key_id = current_user.api_key.id
-      return_data = {:message => "Great Success! Your request to register an app to acess the DPLA API was granted.", :application => application, :apiKeyId => api_key_id}
+      return_data = {
+        :message => "Great Success! Your request to register an app to acess the DPLA API was granted.",
+        :application => application,
+        :apiKeyId => api_key_id
+      }
     else
       status = 403
       return_data = {:message => "There was a problem processing this request. Please try again later."}
@@ -21,10 +25,17 @@ class ApiKeysController < ActionController::Base
     return_data = {:message => "There was a problem requesting your access token. Check your password and try again."}
     status = 403
     if params[:authorize].present?
-       api_key = ApiKey.find(params[:id])
-       token = api_key.authorize_app("#{request.protocol}#{request.env["SERVER_NAME"]}:#{request.env["SERVER_PORT"]}", params[:password])
-       return_data, status = {:message => "Your Access Token Has Been Granted", :token => token.token}, 200 unless token.nil?
+      api_key = ApiKey.find(params[:id])
+      token = api_key.authorize_app(root_url, params[:password])
+      if !token.nil?
+        return_data = {:message => "Your Access Token Has Been Granted", :token => token.token}
+        status = 200
+      end
     end
     render :json => return_data.to_json, :status => status
+  end
+
+  def index
+    render :layout => "application"
   end
 end
