d48e3a416459c62c62af20c03ea6ae0d5812ca1b
Barrett Griffith <barrett@endpoint.com>
2012-10-25 17:44:41 +0200

    Add loading of dataset to cucumber helper

--- features/step_definitions/standard_test_dataset_steps.rb
+++ features/step_definitions/standard_test_dataset_steps.rb
@@ -3,7 +3,7 @@ Given /^the dataset exists$/ do
 end
 
 When /^I have valid JSON in the test dataset$/ do
-  @json = File.read(File.dirname(__FILE__) + "/../../v1/lib/v1/standard_dataset/items.json")
+  @json = load_dataset
 end
 
 Then /^I should not get a dataset error$/ do
@@ -13,14 +13,14 @@ Then /^I should not get a dataset error$/ do
 end
 
 Given /^there are (\d+) items that contain the word "(.*?)" in the "(.*?)"$/ do |arg1, arg2, arg3|
-  @json = File.read(File.dirname(__FILE__) + "/../../v1/lib/v1/standard_dataset/items.json")
+  @json = load_dataset
   dataset = JSON.parse(@json)
   matched_key_value_count = dataset.inject(0) {|count, el| count +=1 if el[arg3].scan(/#{arg2}/i).any?; count}
   expect(matched_key_value_count.to_s).to eq(arg1)
 end
 
 Given /^there is a metadata record "(.*?)" with "(.*?)" in the "(.*?)" field$/ do |arg1, arg2, arg3|
-  @json = File.read(File.dirname(__FILE__) + "/../../v1/lib/v1/standard_dataset/items.json")
+  @json = load_dataset
   dataset = JSON.parse(@json)
   grouped_dataset = dataset.group_by {|el| el["_id"]}
   expect(grouped_dataset.include?(arg1)).to be(true)
