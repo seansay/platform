a46304580b093f21eea0cf96743cf8cd4787396f
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2013-08-28 18:22:13 -0400

    Remove backslash from metacharacter list

--- v1/lib/v1/searchable/query.rb
+++ v1/lib/v1/searchable/query.rb
@@ -10,7 +10,7 @@ module V1
 
       # not escaped, but probably could be: '&&', '||'
       # not escaped, because they don't seem to need it: '+', '-',
-      ESCAPED_METACHARACTERS = [ '"', '!', '(', ')', '{', '}', '[', ']', '^', '~', '?', ':', '\\' ]
+      ESCAPED_METACHARACTERS = [ '"', '!', '(', ')', '{', '}', '[', ']', '^', '~', '?', ':',  ] #'\\'
 
       def self.execute_empty_search(search)
         # We need to be explicit with an empty search
