25f8ba74f2dcf6a9790450db9cfc4c7ba45c7a56
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2014-01-28 18:06:18 -0500

    Add view compilation and --only ... switch to compact-bigcouch.rb

--- bin/compact-bigcouch.rb
+++ bin/compact-bigcouch.rb
@@ -7,9 +7,9 @@ require 'net/http'
 require 'getoptlong'
 
 opts = GetoptLong.new(
-#  [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
   [ '--database', '-d', GetoptLong::REQUIRED_ARGUMENT ],
   [ '--shard', '-s', GetoptLong::REQUIRED_ARGUMENT ],  # Example: e0000000-ffffffff
+  [ '--only', '-o', GetoptLong::REQUIRED_ARGUMENT ],  # --only database, or --only view
 )
 
 params = {}
@@ -22,6 +22,7 @@ def get(url)
 end
 
 def post(url, request_headers)
+  # url can be a string or a URI instance
   #TODO: convert to taking a full URI instance
   uri = URI(url)
   req = Net::HTTP::Post.new(uri.path)
@@ -42,17 +43,22 @@ def post(url, request_headers)
   end
 end
 
-# curl -H "Content-Type: application/json" -X POST http://USER:PASS@repo-node3:5986
-# /shards%2Fe0000000-ffffffff%2Fdashboard.1372038665/_compact/all_provider_docs
 
-def get_shards(host)
-  JSON.parse(get(host + '_all_dbs'))
+def get_shards(uri)
+  JSON.parse(get(uri.to_s + '_all_dbs'))
 end
 
 def run_view_cleanup(database_url)
-  post(database_url + '/_view_cleanup', {})
+  post(database_url.to_s + '/_view_cleanup', {})
 end
 
+def get_backdoor_uri(repo_uri)
+  uri = repo_uri.dup
+  uri.path = '/'
+  uri.port += 2
+  uri
+end
+  
 def main(params)
   repo_uri = URI.parse(params['database'])
   request_headers = {
@@ -60,38 +66,73 @@ def main(params)
     :password => repo_uri.password
   }
   
-  target_database = repo_uri.path.match(/^\/(.+)$/)[1]
-  backdoor_host = 'http://' + repo_uri.host + ':' + (repo_uri.port + 2).to_s + '/'
-  frontdoor_host = 'http://' + repo_uri.host + ':' + (repo_uri.port).to_s + '/'
-  compact_api_prefix = backdoor_host + 'shards%2F'
+  target_database = repo_uri.path.split('/')[1]
+  backdoor_host = get_backdoor_uri(repo_uri)
+  frontdoor_host = repo_uri.dup
+  frontdoor_host.path = '/'
 
-  if compact_running(frontdoor_host)
-    puts "Compaction already running on #{repo_uri.host}: #{compact_running(frontdoor_host)}"
+  if compaction_status(frontdoor_host)
+    puts "Compaction already running on #{repo_uri.host}: #{compaction_status(frontdoor_host)}"
     exit 0
   end
 
   # Always start with view compaction for this database
-  run_view_cleanup(frontdoor_host + target_database)
+  run_view_cleanup(repo_uri)
+  design_docs = get_design_docs(frontdoor_host, target_database).shuffle
 
   get_shards(backdoor_host).shuffle.each do |path|
     next unless path =~ /^shards\/(.+)\/(#{target_database}\.\d+)$/
     shard, database = $1, $2
-
-    # limit to requested shard if there was one
     next if params['shard'] && params['shard'] != shard
 
-    url = compact_api_prefix + shard + '%2F' + database + '/_compact'
-    logmsg "Compacting database shard #{shard}/#{database} -> #{url}"
+    base_url = backdoor_host.to_s + 'shards%2F' + shard + '%2F' + database + '/_compact'
+    target_urls = []
 
-    post(url, request_headers)
+    if params['only'] != 'view'
+      target_urls << base_url
+    end
+    if params['only'] != 'database'
+      target_urls.concat design_docs.map {|doc| base_url + '/' + doc}
+    end
 
-    while true do
-      sleep 5
-      running = compact_running(frontdoor_host)
-      break if running.nil?
-      logmsg running
+    target_urls.flatten.each do |url|
+      logmsg "Compacting: #{url}"
+      post(url, request_headers)
+      loop_delay(frontdoor_host)
     end
     
+  end
+
+end
+
+def get_design_docs(host, database)
+  list_url = host.to_s + database + '/_all_docs?startkey=%22_design/%22&endkey=%22_design0%22'
+  JSON.parse(get(list_url))['rows'].map do |hash|
+    hash['key'].sub(/^_design\//, '')
+  end
+end
+
+def loop_delay(frontdoor_host)
+  start_time = Time.now.to_i
+  while true do
+    sleep 5
+    status = compaction_status(frontdoor_host, start_time)
+    break if status.nil?
+
+    logmsg status
+  end
+end
+
+def compute_eta(start_time, message)
+  # Compute a rough ETA. The math only really makes sense for database compactions, not views
+  if message =~ /\((\d+)%\)/
+    return if $1.to_i == 0
+    
+    percentage = $1.to_i * 0.01
+    elapsed = Time.now.to_i - start_time
+    total_runtime = elapsed / percentage  #total time this run should take
+
+    return (total_runtime - elapsed).to_i
   end
 end
 
@@ -99,16 +140,14 @@ def logmsg(msg)
   puts "[#{Time.now.to_s}] #{msg}"
 end
 
-def compact_running(base_endpoint)
-  # Do not use the more obvious API endpoint because it lies 5% of
-  # the time: JSON.parse(HTTParty.get(endpoint))['compact_running']
-  json = JSON.parse(get(base_endpoint + '_active_tasks'))
-  #view comapction looks like this: [{"type":"View Group Compaction","task":"shards/e0000000-ffffffff/dashboard.1372038665/all_provider_docs","status":"Copied 100000 of 1788853 Ids (5%)","pid":"<0.3620.746>"}]
+def compaction_status(base_endpoint, start_time=nil)
+  # This assumes that your bigcouch node is set to include that node's `hostname`
+  # value in that node's vm.args config value for the -name config variable
+  hostname = base_endpoint.host
+  json = JSON.parse(get(base_endpoint.to_s + '_active_tasks'))
+  entry = json.detect {|x| x['node'] =~ /\b#{hostname}\b/ && x['type'] =~ /Compaction$/}
 
-  hostname = URI.parse(base_endpoint).host
-  entry = json.detect {|x| x['node'] =~ /\b#{hostname}\b/ && x['type'] == 'Database Compaction'}
-
-  entry.nil? ? nil : "#{ entry['task'] } - #{entry['status'] }"
+  entry.nil? ? nil :  "#{ entry['task'] } - #{entry['status'] }"
 end
 
 main(params)
